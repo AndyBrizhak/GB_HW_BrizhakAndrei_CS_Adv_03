fe18e73df7e7a69f77d224341414395f51e3f5a5
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-06 16:47:05 +0800

    + класс Healthpack : Asteroid + свойство  Power = -10;

---
+++ AsteroidsGame/Healthpack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteroidsGame
+{
+    class Healthpack : Asteroid
+    {
+        public Healthpack(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+            Power = -10;
+        }
+    }
+}
